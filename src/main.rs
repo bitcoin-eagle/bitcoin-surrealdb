9e8ed29343593560ca12a68b9067da86c554111c
Bitcoin Eagle <github.bz24ml@bitcoineagle.dev>
2023-10-13 13:23:50 +0800

    split script pubkey and address

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 use anyhow::Result;
 use bitcoin::{
     address::{Payload, WitnessVersion},
-    Address, AddressType, Block, Network, Script,
+    Address, Block, Network, Script,
 };
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use serde::Deserialize;
@@ -21,11 +21,14 @@ const TIP_HIST_TABLE: &str = "tip_hist";
 const BLOCK_TABLE: &str = "block";
 const TRANSACTION_TABLE: &str = "transaction";
 const TXOUT_TABLE: &str = "tx_out";
+const SCRIPT_PUBKEY_TABLE: &str = "script_pubkey";
 const ADDRESS_TABLE: &str = "address";
 const OUTPUTS_EDGE: &str = "outputs";
 const SPENT_BY_EDGE: &str = "spent_by";
 const REWARDS_EDGE: &str = "rewards";
 const CONFIRMS_EDGE: &str = "confirms";
+const LOCKED_BY_EDGE: &str = "locked_by";
+const AS_ADDRESS_EDGE: &str = "as_address";
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -57,20 +60,25 @@ async fn run() -> Result<()> {
     // delete all records from blocks
     let mut r = db
         .query(format!(
-            "DELETE {};DELETE {};DELETE {};DELETE {};DELETE {};DELETE {};DELETE {};",
+            "DELETE {};DELETE {};DELETE {};DELETE {};DELETE {};DELETE {};DELETE {}; DELETE {}; DELETE {}; DELETE {}; DELETE {}; DELETE {};",
             OUTPUTS_EDGE,
             SPENT_BY_EDGE,
+            CONFIRMS_EDGE,
+            LOCKED_BY_EDGE,
+            REWARDS_EDGE,
+            AS_ADDRESS_EDGE,
             TRANSACTION_TABLE,
             TIP_HIST_TABLE,
             BLOCK_TABLE,
             TXOUT_TABLE,
-            ADDRESS_TABLE
+            ADDRESS_TABLE,
+            SCRIPT_PUBKEY_TABLE,
         ))
         .await?;
     for e in r.take_errors() {
         Err(e.1)?;
     }
-    assert_eq!(r.num_statements(), 7);
+    assert_eq!(r.num_statements(), 12);
 
     let btc = Client::new(
         "localhost:8332",
@@ -87,6 +95,7 @@ async fn run() -> Result<()> {
     println!("block info:\n{:?}", header);
     let mut height = 0;
     let mut buf = String::new();
+    let max_height = header.height as u64;
     let network = Network::from_core_arg(blockchain_info.chain.as_str())?;
     while let Ok(block_hash) = btc.get_block_hash(height) {
         println!("block [{}]:{}", height, block_hash);
@@ -166,50 +175,119 @@ fn block_to_surql(buf: &mut String, height: u64, block: Block, network: Network)
         for (output, vout) in transaction.output.into_iter().zip(0_u32..) {
             let txout_id = prepare_txout_id(&txid_raw_str, vout);
 
+            let script_pubkey_id = {
+                let mut script_pubkey_id = String::new();
+                push_id_str_disp(
+                    &mut script_pubkey_id,
+                    SCRIPT_PUBKEY_TABLE,
+                    output.script_pubkey.wscript_hash(),
+                );
+                script_pubkey_id
+            };
             buf.push_str("UPDATE ");
             buf.push_str(&txout_id);
             buf.push_str(" CONTENT {");
             push_pair_raw_disp(buf, "value_sats", output.value);
-
-            let (out_type, address) = parse_script_pubkey(&output.script_pubkey, network);
-            let address_id = {
-                let mut address_id = String::new();
-                push_id_str(&mut address_id, ADDRESS_TABLE, &address);
-                address_id
-            };
-            push_pair_raw(buf, "address_id", &address_id);
-
             buf.push_str("} RETURN NONE;\n");
 
+            // TODO: INSERT SCRIPT_PUBKEY
             buf.push_str("UPDATE ");
-            buf.push_str(&address_id);
+            buf.push_str(&script_pubkey_id);
             buf.push_str(" CONTENT {");
-            push_pair_str(
-                buf,
-                "type",
-                match out_type {
-                    TxOutType::Address(ref addr) => addr.to_string(),
-                    _ => Into::<&'static str>::into(&out_type).to_string(),
-                },
-            );
-            //push_pair_str(buf, "type", format!("{:?}", &out_type));
             push_pair_str_disp(
                 buf,
                 "script_pubkey_hash",
                 output.script_pubkey.script_hash(),
-            );
-            push_pair_str_disp(
-                buf,
-                "script_pubkey_wshash",
-                output.script_pubkey.wscript_hash(),
             );
             push_pair_str(
                 buf,
                 "script_pubkey_hex",
                 output.script_pubkey.to_hex_string(),
             );
-            push_pair_str_disp(buf, "script_pubkey", output.script_pubkey);
+            push_pair_str(
+                buf,
+                "script_pubkey_asm",
+                &output.script_pubkey.to_asm_string(),
+            );
+            push_pair_raw_disp(
+                buf,
+                "provably_unspendable",
+                output.script_pubkey.is_provably_unspendable(),
+            );
             buf.push_str("} RETURN NONE;\n");
+
+            // TODO: RELATE TX_OUT -> locked_by -> SCRIPT_PUBKEY
+            buf.push_str("RELATE ");
+            buf.push_str(&txout_id);
+            buf.push_str("->");
+            buf.push_str(LOCKED_BY_EDGE);
+            buf.push_str("->");
+            buf.push_str(&script_pubkey_id);
+            buf.push_str(" CONTENT {");
+            buf.push_str("} RETURN NONE;\n");
+
+            if let Ok(address) = Address::from_script(&output.script_pubkey, network) {
+                if let Some(t) = address.address_type() {
+                    let address_id = {
+                        let mut address_id = String::new();
+                        push_id_str_disp(&mut address_id, ADDRESS_TABLE, &address);
+                        address_id
+                    };
+                    buf.push_str("UPDATE ");
+                    buf.push_str(&address_id);
+                    buf.push_str(" CONTENT {");
+                    push_pair_str(buf, "type", t.to_string().to_uppercase());
+                    buf.push_str("} RETURN NONE;\n");
+
+                    buf.push_str("RELATE ");
+                    buf.push_str(&txout_id);
+                    buf.push_str("->");
+                    buf.push_str(AS_ADDRESS_EDGE);
+                    buf.push_str("->");
+                    buf.push_str(&address_id);
+                    buf.push_str(" CONTENT {");
+                    buf.push_str("} RETURN NONE;\n");
+                } else {
+                    // INFO: non-standard future segwit version
+                }
+            }
+
+            // let (out_type, address) = parse_script_pubkey(&output.script_pubkey, network);
+            // let address_id = {
+            //     let mut address_id = String::new();
+            //     push_id_str(&mut address_id, ADDRESS_TABLE, &address);
+            //     address_id
+            // };
+
+            // buf.push_str("UPDATE ");
+            // buf.push_str(&address_id);
+            // buf.push_str(" CONTENT {");
+            // push_pair_str(
+            //     buf,
+            //     "type",
+            //     match out_type {
+            //         TxOutType::Address(ref addr) => addr.to_string(),
+            //         _ => Into::<&'static str>::into(&out_type).to_string(),
+            //     },
+            // );
+            // //push_pair_str(buf, "type", format!("{:?}", &out_type));
+            // push_pair_str_disp(
+            //     buf,
+            //     "script_pubkey_hash",
+            //     output.script_pubkey.script_hash(),
+            // );
+            // push_pair_str_disp(
+            //     buf,
+            //     "script_pubkey_wshash",
+            //     output.script_pubkey.wscript_hash(),
+            // );
+            // push_pair_str(
+            //     buf,
+            //     "script_pubkey_hex",
+            //     output.script_pubkey.to_hex_string(),
+            // );
+            // push_pair_str_disp(buf, "script_pubkey", output.script_pubkey);
+            // buf.push_str("} RETURN NONE;\n");
 
             buf.push_str("RELATE ");
             buf.push_str(&transaction_id);
@@ -244,7 +322,7 @@ enum TxOutType {
     P2pk,
     P2ms,
     OpReturn,
-    Address(AddressType),
+    Address,
     NonStandardWitness(WitnessVersion),
     NonStandard,
 }
@@ -252,7 +330,7 @@ enum TxOutType {
 fn parse_script_pubkey(script_pubkey: &Script, network: Network) -> (TxOutType, String) {
     if let Ok(address) = Address::from_script(script_pubkey, network) {
         let out_type = if let Some(out_type) = address.address_type() {
-            TxOutType::Address(out_type)
+            TxOutType::Address
         } else if let Payload::WitnessProgram(ref prog) = address.payload {
             TxOutType::NonStandardWitness(prog.version())
         } else {
